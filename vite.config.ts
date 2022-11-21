ed7398a8f78cd83d2dd8e060c243e14483fb218a
machangzhi <284551878@qq.com>
2022-11-21 22:44:40 +0800

    引入dotenv并配置dev环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import dotenv, { DotenvParseOutput } from 'dotenv';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+// export default defineConfig({
+//   plugins: [vue()]
+// });
+
+export default defineConfig((mode) => {
+  console.log('打包编译阶段还是编码阶段：', mode.command);
+  console.log('当前在什么环境运行项目:', mode.mode);
+
+  //  拼接当前环境文件名。
+  const envFileName: string = '.env';
+  const curEnvFileName = `${envFileName}.${mode.mode}`;
+
+  // fs.readFileSync：读取环境文件key-value数据到缓存对象。
+  // dotenv.parse 读取缓存对象到envConf对象中。
+  const envConf: DotenvParseOutput = dotenv.parse(fs.readFileSync(curEnvFileName));
+
+  const curEnv: string = mode.mode;
+  let server: Record<string, any> = {};
+  if (curEnv === "development") { // 开发环境
+    server = {
+      port: envConf.VITE_PORT,
+      host: envConf.VITE_HOST,
+      proxy: {
+        [envConf.VITE_BASE_URL]: {
+          target: envConf.VITE_PROXY_DOMAIN,
+        },
+      }
+    };
+  } else if (curEnv === "production") {// 生产环境
+    server = {
+      port: envConf.VITE_PORT,
+      host: envConf.VITE_HOST,
+    };
+  }
+  return {
+    plugins: [vue()],
+  };
 });
