38e179848386b5a0acf344c32c41f49cc84bcdaf
machangzhi <284551878@qq.com>
2022-11-21 22:07:01 +0800

    新增环境变量配置文件

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,12 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+interface ImportMetaEnv {
+  VITE_username: String,
+  VITE_age: Number;
 }
